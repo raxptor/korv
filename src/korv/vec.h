2a30112228b5757515743f6a6f0b1059626c62d3
Dan Nilsson <raxptor@gmail.com>
2016-01-27 12:05:01 +0100

    - more

---
+++ src/korv/vec.h
@@ -0,0 +1,23 @@
+#ifndef __KORV_VEC_H__
+#define __KORV_VEC_H__
+
+namespace korv
+{
+	/////////////////////////////////////////////
+	// Yes, this is all you get, so deal with it.
+
+	typedef float mat4f[16];
+	typedef float vec4f[4];
+	typedef float vec3f[3];
+	typedef float vec2f[2];
+
+	/*
+	Not supported yet
+	typedef double mat4d[16];
+	typedef double vec4d[4];
+	typedef double vec3d[3];
+	typedef double vec2d[2];
+	*/
+}
+
+#endif
